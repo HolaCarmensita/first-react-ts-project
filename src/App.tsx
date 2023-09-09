492a32e4573a8b0732e2c7fbdce7ceef18effe7d
HolaCarmensita <Casandra.c.gustafsson@gmail.com>
2023-09-09 15:25:19 +0200

    the first test with mirage and react ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TodoList from './components/TodoList';
 import NewTodo from './components/NewTodo';
 import { Todo } from './todo.model';
+import { createServer } from 'miragejs';
+import data from './data.json';
+
+createServer({
+  routes() {
+    this.get('api/users', () => {
+      return data;
+    });
+  },
+});
+
+/* flytta till egen fil */
+
+type User = {
+  id: number;
+  name: string;
+};
+/* flytta till egen fil */
 
 const App: React.FC = () => {
+  /* flytta till egen fil */
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    fetch('api/users')
+      .then((res) => res.json())
+      .then((users) => setUsers(users));
+  }, []);
+  /* flytta till egen fil */
+
   const [todos, setTodos] = useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
@@ -19,9 +47,13 @@ const App: React.FC = () => {
     });
   };
 
+  <div></div>;
+
   return (
     <div className='App'>
-      {/* A component that adds todos */}
+      {users.map((user) => (
+        <div key={user.id}>{user.name}</div>
+      ))}
       <NewTodo onAddTodo={todoAddHandler} />
       <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
     </div>
